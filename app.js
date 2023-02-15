9c7f46509c1448a4446fcfb6ed591a32c19f44dd
Bnyro <bnyro@tutanota.com>
2023-02-15 20:04:09 +0100

    Auto scrolling on new line

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const textContainer = $("#text");
 const maxWordLength = 5;
 let words = [];
 let currentIndex = 0;
+let currentLine = 0;
+let lines = [];
 
 const appendEl = (parentElement, text) => {
   const newEl = document.createElement("span");
@@ -22,14 +24,32 @@ const fetchWords = async () => {
   words = text.split("\n").filter((word) => word.length <= maxWordLength);
 };
 
+const getLines = () => {
+  const chars = [...$$("#text span")];
+  const groups = groupBy(chars, (el) => el.offsetTop);
+  return [...groups.entries()];
+};
+
+const updateLines = () => {
+  const prevLines = lines.slice(0, currentLine + 1);
+  const length = prevLines.map((el) => el[1].length).reduce((a, b) => a + b, 0);
+  if (currentIndex == length) {
+    prevLines[currentLine][1].forEach((span) => {
+      span.hidden = true;
+    });
+    currentLine += 1;
+  }
+};
+
 const showText = () => {
-  range(50).forEach((_) => {
+  range(1000).forEach((_) => {
     const chars = [...words.random()];
     chars.forEach((char) => {
       appendEl(textContainer, char);
     });
   });
   currentEl().classList.add("current");
+  lines = getLines();
 };
 
 fetchWords().then(() => {
@@ -45,6 +65,8 @@ document.addEventListener("keypress", (event) => {
     current.classList.add("correct");
     current.classList.remove("current");
     currentIndex += 1;
+    currentEl().classList.add("current");
+    updateLines();
   }
 });
 
@@ -58,4 +80,18 @@ function isLetter(char) {
 
 function currentEl() {
   return $$("#text span")[currentIndex];
+}
+
+function groupBy(list, keyGetter) {
+  const map = new Map();
+  list.forEach((item) => {
+    const key = keyGetter(item);
+    const collection = map.get(key);
+    if (!collection) {
+      map.set(key, [item]);
+    } else {
+      collection.push(item);
+    }
+  });
+  return map;
 }
